5c041426389e29d13d522c41ebde7f46782bfd24
Louis-Philippe Gauthier <lpgauth@gmail.com>
2015-09-04 08:57:57 +0530

    Use ms_base

--- bin/dev.sh
+++ bin/dev.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-exec erl -sname ms_http \
+exec erl -sname ms_http_dev \
          -pa ebin deps/*/ebin test \
          -config config/dev \
          -boot start_sasl \
-         -s ms_http_app
+         -s ms_http_app \
+         -setcookie secret
