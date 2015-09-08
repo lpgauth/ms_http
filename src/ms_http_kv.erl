590bddcf5987cbcfa9b4c9d582f75bc929e1386e
Louis-Philippe Gauthier <lpgauth@gmail.com>
2015-09-08 17:05:26 +0530

    Fix spec

--- src/ms_http_kv.erl
+++ src/ms_http_kv.erl
@@ -14,11 +14,10 @@
 get(Key) ->
     ms_base:apply(ms_kv, ms_kv, get, [Key]).
 
--spec put(binary(), binary()) -> ok.
-
+-spec put(binary(), binary()) -> [ok].
 
 put(Key, Value) ->
-    ms_base:apply(ms_kv, ms_kv, put, [Key, Value]).
+    ms_base:apply_all(ms_kv, ms_kv, put, [Key, Value]).
 
 -else.
 
