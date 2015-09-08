9fc4ccd3b329398cebf6a572747745a7d2f0ffab
Louis-Philippe Gauthier <lpgauth@gmail.com>
2015-09-09 04:38:41 +0530

    Add use apply_hash for cache locality

--- src/ms_http_kv.erl
+++ src/ms_http_kv.erl
@@ -12,12 +12,12 @@
 -spec get(binary()) -> {ok, binary()} | not_found.
 
 get(Key) ->
-    ms_base:apply(ms_kv, ms_kv, get, [Key]).
+    ms_base:apply_hash(ms_kv, Key, ms_kv, get, [Key]).
 
 -spec put(binary(), binary()) -> [ok].
 
 put(Key, Value) ->
-    ms_base:apply(ms_kv, ms_kv, put, [Key, Value]).
+    ms_base:apply_hash(ms_kv, Key, ms_kv, put, [Key, Value]).
 
 -else.
 
